08c09792fb85adbcda260d76ef3dc64491190baf
Giousa <65489469@qq.com>
2022-11-15 15:06:49 +0800

    移除布局控件

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,6 +9,7 @@
 #include <QDebug>
 #include <QSettings>
 #include "webwidget.h"
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -63,6 +64,7 @@ void MainWindow::parseConfigFile(QString path)
 
     if(!file.open(QIODevice::ReadOnly)){
          qDebug()<<"文件打开失败!";
+         QMessageBox::warning(this,"警告","文件打开失败");
         return;
     }
 
@@ -75,24 +77,42 @@ void MainWindow::parseConfigFile(QString path)
     QJsonDocument doc = QJsonDocument::fromJson(data,&parseError);
     if(parseError.error != QJsonParseError::NoError){
         qDebug()<<"文件配置错误";
+        QMessageBox::warning(this,"警告","文件配置错误");
         return;
     }
 
+
+    //清空horizontalLayout布局内的所有元素
+    QLayoutItem *child;
+    while ((child = ui->gridLayout->takeAt(0)) != 0)
+    {
+        //setParent为NULL，防止删除之后界面不消失
+        if(child->widget())
+        {
+            child->widget()->setParent(NULL);
+            delete child->widget();//释放
+        }
+
+        delete child;
+    }
+
+
+    //解析并生成按钮
     QJsonArray toolArray = doc.array();
-    qDebug() << "toolArray:  " << toolArray;
+//    qDebug() << "toolArray:  " << toolArray;
 
     for (int i = 0;i < toolArray.size() ;i++ ) {
         QJsonObject object = toolArray.at(i).toObject();
         QString toolName = object.take("toolName").toString();
         QString toolURL = object.take("toolURL").toString();
 
-        qDebug() << "toolName: " << toolName << ",toolURL: " << toolURL;
+//        qDebug() << "toolName: " << toolName << ",toolURL: " << toolURL;
 
         QPushButton * btn = new QPushButton(toolName,this);
         btn->setMaximumSize(160, 40);
 
         int offset = i/offsetNum;
-        qDebug() << "offset = " << offset;
+//        qDebug() << "offset = " << offset;
         ui->gridLayout->addWidget(btn,offset,i-offsetNum*offset);
 
         connect(btn,&QPushButton::clicked,this,[=](){buttonClicked(toolName,toolURL);});
@@ -104,7 +124,7 @@ void MainWindow::parseConfigFile(QString path)
 
 void MainWindow::buttonClicked(QString toolName, QString toolURL)
 {
-    qDebug() << "点击： " << toolName << ", 链接： " << toolURL;
+//    qDebug() << "点击： " << toolName << ", 链接： " << toolURL;
 
     WebWidget *widget = new WebWidget(this,toolName,toolURL);
     widget->setAttribute(Qt::WA_DeleteOnClose);
